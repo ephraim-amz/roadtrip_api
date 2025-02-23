801cecedf39bb76c43c9d3040cbe1a2827c19bba
ephraim-amz <ephraimamezian@gmail.com>
2025-02-23 15:57:43 +0100

    Minor modification of pom.xml file + deletion of unused java file

--- src/main/java/com/example/roadtrip_api/RoadtripApiApplication.java
+++ src/main/java/com/example/roadtrip_api/RoadtripApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoadtripApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoadtripApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoadtripApiApplication.class, args);
+    }
 
 }
