a24b51f305f8267c3e6141ee8315a52247e76c14
ephraim-amz <ephraimamezian@gmail.com>
2025-02-23 19:04:39 +0100

    test : creation of ActivityControllerTest file

---
+++ src/test/java/com/example/roadtrip_api/ActivityControllerTest.java
@@ -0,0 +1,8 @@
+package com.example.roadtrip_api;
+
+import com.example.roadtrip_api.activity.ActivityController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ActivityController.class)
+class ActivityControllerTest {
+}
