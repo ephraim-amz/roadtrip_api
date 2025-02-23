bfe0d98d5db731acab0304becabff78cf52a472b
ephraim-amz <ephraimamezian@gmail.com>
2025-02-23 19:15:25 +0100

    test : addition of createActivityTest test case

--- src/test/java/com/example/roadtrip_api/ActivityControllerTest.java
+++ src/test/java/com/example/roadtrip_api/ActivityControllerTest.java
@@ -1,8 +1,37 @@
 package com.example.roadtrip_api;
 
 import com.example.roadtrip_api.activity.ActivityController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ActivityController.class)
 class ActivityControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void createActivityTest() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/activity/new")
+                        .content("{\n" +
+                                "    \"name\": \"Parisian Food Walking Tour\",\n" +
+                                "    \"description\": \"Sample the finest French delicacies while exploring charming neighborhoods, including cheese, wine, and pastry tastings.\",\n" +
+                                "    \"city\": \"Paris\",\n" +
+                                "    \"duration\": 180,\n" +
+                                "    \"price\": 95,\n" +
+                                "    \"category\": \"food\",\n" +
+                                "    \"rating\": 4.7\n" +
+                                "}").contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
 }
